ce35b0556c2d8b45936b7924788c55ba8c89864a
cilingir <144216029+tolgacilingir@users.noreply.github.com>
2024-01-25 18:41:47 +0300

    Add files via upload

---
+++ calculator.py
@@ -0,0 +1,20 @@
+def calc(x,y,ops):
+
+	if ops not in "+-/*":
+		return("Only +-/* !!!")
+
+	if ops == "+":
+		return(str(x) + " " + ops + " " + str(y) + "=" + str(x+y))
+	elif ops == "-":
+	    return(str(x) + " " + ops + " " + str(y) + "=" + str(x+y))
+	elif ops == "*":
+	    return(str(x) + " " + ops + " " + str(y) + "=" + str(x+y))    	
+	elif ops == "/":
+	    return(str(x) + " " + ops + " " + str(y) + "=" + str(x+y))   
+while True:	    
+
+	x = int(input("please enter first number: "))
+	y = int(input("please enter second number: "))
+	ops = int(input("Choose between x,-,*,/ "))
+
+	print(calc(x,y,ops))
